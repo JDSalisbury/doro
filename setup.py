887bf1b0b789bb96c23cf1d7ba1db651b876a886
Jeff <salisbury.jeffery@gmail.com>
2021-11-25 17:54:52 -0500

    Testing Pomodoro time and short and long break intervals.

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
     requirements = fh.read()
 
 setup(
-    name = 'Doro',
-    version= '0.0.1',
+    name='Doro',
+    version='0.0.1',
     author='Jeff Salisbury',
     author_email='salisbury.jeffery@gmail.com',
     license='MIT',
@@ -16,15 +16,15 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/JDSalisbury/doro',
-    py_modules= ['my_tool', 'app'],
-    packages= find_packages(),
-    install_requires = [requirements],
+    py_modules=['my_tool', 'app'],
+    packages=find_packages(),
+    install_requires=[requirements],
     python_requires='>=3.7',
     classifiers=[
         'Programing Language :: Python :: 3.8',
         "Operating System :: OS independent",
     ],
-    entry_points = '''
+    entry_points='''
         [console_scripts]
         doro=timer:cli
     '''
--- timer.py
+++ timer.py
@@ -14,37 +14,40 @@ def cli():
 @click.option('-s', '--short', default="5", help='Short break length')
 @click.option('-l', '--long', default="25", help='Long preak taken after 4 focus+short pomodoro intervals.')
 def custom(focus, short, long):
-    ...
+    click.echo(f'Focus: {focus}')
+    click.echo(f'Short: {short}')
+    click.echo(f'Long: {long}')
 
 
 @cli.command()
 def start():
-    t = Timer()
-    t.start()
-    click.echo("Starting work Session")
-    timer_flag = 0
-    while timer_flag < 1:
-        print("This prints once every 5 seconds.")
-        time.sleep(5)
-        timer_flag += 1
+    long_breaks = 4
+    short_breaks = 4
 
-    t.stop()
-    bk = Timer()
-    bk.start()
-    playsound('./app/alarm.mp3')
-    while bk.current_time() < 57:
-        print("Take a break!")
-        # print(bk.current_time())
+    for _l in range(long_breaks):
+        for _s in range(short_breaks):
+            t = Timer()
+            t.start()
+            click.echo(f"Starting work Session {_s +1} of {short_breaks}")
+            timer_flag = 0
+            while timer_flag < 1:
+                print("This prints once every 5 seconds.")
+                time.sleep(5)
+                timer_flag += 1
 
-    tn = Timer()
-    playsound('./app/startBeep.mp3')
-    tn.start()
-    click.echo("Starting Second work Session")
-    timer_flag = 0
-    while timer_flag < 1:
-        print("This prints once every 5 seconds.")
-        time.sleep(5)
-        timer_flag += 1
+            t.stop()
+            bk = Timer()
+            bk.start()
+            playsound('./app/valarm.mp3')
+            while bk.current_time() < 17:
+                print("Take a short break!")
+            playsound('./app/startBeep.mp3')
 
-    tn.stop()
-    playsound('./app/alarm.mp3')
+        playsound('./app/lalarm.mp3')
+        click.echo(f'Starting long break! {_l +1} of {long_breaks}')
+        lb = Timer()
+        lb.start()
+        playsound('./app/valarm.mp3')
+        while lb.current_time() < 27:
+            print("Take a long break!")
+        playsound('./app/startBeep.mp3')
