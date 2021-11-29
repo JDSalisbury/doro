15c1dc903e01b9fcc420b5cc26bde22c9dc3531a
Jeff <salisbury.jeffery@gmail.com>
2021-11-29 11:58:24 -0500

    Refactoring main file to remove busy logic from runner file. moved sounds to its own folder within app.

---
+++ app/application.py
@@ -0,0 +1,38 @@
+import click
+import time
+from app.timer import Timer
+from playsound import playsound
+
+
+def run_timer(minutes, msg):
+    t = Timer()
+    t.start()
+    while t.current_time() < minutes:
+        click.echo(msg)
+        time.sleep(1)
+    t.stop()
+
+
+def start_session(_s, short_breaks, focus):
+    click.echo(f"Starting work Session {_s +1} of {short_breaks}")
+    run_timer(focus, "Focus...")
+
+
+def short_break(_s, short_breaks, short):
+    playsound('./alarms/valarm.mp3')
+    click.echo(f'Starting long break! {_s +1} of {short_breaks}')
+    run_timer(short, "Take a short break!")
+    if (_s + 1) != short_breaks:
+        playsound('./alarms/startBeep.mp3')
+
+
+def long_break(_l, long_breaks, long):
+    click.echo(f'Starting long break! {_l +1} of {long_breaks}')
+    run_timer(long, "Take a long break!")
+    if (_l+1) != long_breaks:
+        playsound('./alarms/startBeep.mp3')
+
+
+def completed():
+    click.echo("Doro Completed")
+    playsound('./alarms/falarm.mp')
--- timer.py
+++ timer.py
@@ -1,40 +1,5 @@
 import click
-import time
-from playsound import playsound
-from app.timer import Timer
-
-
-def run_timer(minutes, msg):
-    t = Timer()
-    t.start()
-    while t.current_time() < minutes:
-        click.echo(msg)
-        time.sleep(1)
-    t.stop()
-
-
-def start_session(_s, short_breaks, focus):
-    click.echo(f"Starting work Session {_s +1} of {short_breaks}")
-    run_timer(focus, "Focus...")
-
-
-def short_break(_s, short_breaks, short):
-    playsound('./app/valarm.mp3')
-    run_timer(short, "Take a short break!")
-    if (_s + 1) != short_breaks:
-        playsound('./app/startBeep.mp3')
-
-
-def long_break(_l, long_breaks, long):
-    click.echo(f'Starting long break! {_l +1} of {long_breaks}')
-    run_timer(long, "Take a long break!")
-    if (_l+1) != long_breaks:
-        playsound('./app/startBeep.mp3')
-
-
-def completed():
-    click.echo("Pomodoro Completed")
-    playsound('./app/falarm.mp')
+from app.application import run_timer, start_session, short_break, long_break, completed
 
 
 @click.group()
@@ -91,4 +56,4 @@ def start(breaks):
 @click.option('-m', '--minutes', default='30', help='Number of minutes')
 def timer(minutes):
     run_timer(int(minutes)*60, 'Counting down...')
-    playsound('./app/falarm.mp')
+    completed()
