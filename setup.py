12cead20dddfc8ec701056f5d33cfb28e1721d69
Jeff Salisbury <salisbury.jeffery@gmail.com>
2022-01-07 09:17:48 -0500

    Version update.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name='Pmdr',
-    version='1.0.1',
+    version='1.0.2',
     author='Jeff Salisbury',
     author_email='salisbury.jeffery@gmail.com',
     license='MIT',
